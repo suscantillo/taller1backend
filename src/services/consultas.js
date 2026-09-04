dea3bbaaf833d8ca8c69d7eeff9942e6331d43f0
jesusckantillo <jesusguerrrero@uninorte.edu.co>
2026-09-03 23:53:39 -0500

    feat funciones consultas basicas

--- src/services/consultas.js
+++ src/services/consultas.js
@@ -1,19 +1,29 @@
 export function vivosHumanos(personajes) {
-  return [];
+  return personajes.filter(function (p) {
+    return p.estado === "Alive" && p.especie === "Human";
+  });
 }
 
 export function conVeinteEpisodiosOMas(personajes) {
-  return [];
+  return personajes.filter(function (p) {
+    return p.cantidadEpisodios >= 20;
+  });
 }
 
 export function primerAlienFemenino(personajes) {
-  return null;
+  return personajes.find(function (p) {
+    return p.especie === "Alien" && p.genero === "Female";
+  });
 }
 
 export function existeAlgunTipo(personajes) {
-  return false;
+  return personajes.some(function (p) {
+    return p.tipo !== "";
+  });
 }
 
 export function todosValidos(personajes) {
-  return false;
+  return personajes.every(function (p) {
+    return p.imagen !== "" && p.cantidadEpisodios >= 1;
+  });
 }
