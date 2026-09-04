45fb8e08f19ce66b5726a54b70049054e1ff354d
Marigugo24 <149719380+Marigugo24@users.noreply.github.com>
2026-09-03 23:38:19 -0500

    feat: normaliza personajes con map

--- src/services/normalizacion.js
+++ src/services/normalizacion.js
@@ -1,3 +1,17 @@
 export function normalizarPersonajes(personajes) {
-  return [];
+  return personajes.map((personaje) => {
+    return {
+      id: p.id,
+      nombre: p.name,
+      estado: p.status,
+      especie: p.species,
+      tipo: p.type,
+      genero: p.gender,
+      origen: p.origin.name,
+      ubicacionActual: p.location.name,
+      cantidadEpisodios: p.episode.length,
+      imagen: p.image,
+    };
+  });
 }
+
